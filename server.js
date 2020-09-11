a2ce84be5671a6e57869f8a17a3d521a63179bac
Michael Liriano <mikethecoder96@gmail.com>
2020-09-11 00:38:39 -0400

    fixed port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '3000';
 const compression = require('compression');
 const helmet = require('helmet');
 const config = require('config');
